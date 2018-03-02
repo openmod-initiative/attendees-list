b05bd02204730f547da3c5f056038a40e0ada25e
Tim Tröndle <tim.troendle@usys.ethz.ch>
2018-03-02 15:57:52 +0100

    Add avatar url retrieval

--- attendees.py
+++ attendees.py
@@ -3,17 +3,20 @@ import requests
 
 
 URL = "https://forum.openmod-initiative.org/"
-AVATAR_URL = "https://forum.openmod-initiative.org/user_avatar/forum.openmod-initiative.org/{username}/120/8_1.png"
+AVATAR_URL = "https://forum.openmod-initiative.org/user_avatar/forum.openmod-initiative.org/{}/120/8_1.png"
 
 USER_REQUEST = URL + "users/{}.json?"
 
 
 def attendee_list(usernames, api_username, api_key):
     users = [_get_user(username, api_username, api_key) for username in usernames]
-    return pd.DataFrame({
-        "name": [user["user"]["name"] for user in users],
-        "username": [user["user"]["username"] for user in users]
-    })
+    return pd.DataFrame(
+        index=[user["user"]["username"] for user in users],
+        data={
+            "name": [user["user"]["name"] for user in users],
+            "avatar_url": [AVATAR_URL.format(username) for username in usernames]
+        }
+    )
 
 
 def _get_user(username, api_username, api_key):
--- test_attendees.py
+++ test_attendees.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 
 import pytest
+import requests
 
 import attendees
 
@@ -11,6 +12,8 @@ TEST_USERS = [
     User(username="timtroendle", name="Tim Tröndle"),
     User(username="tom_brown", name="Tom Brown")
 ]
+
+VALID_IMAGE_MIME_TYPES = ["image/png", "image/jpeg"]
 
 
 @pytest.fixture(params=TEST_USERS)
@@ -24,13 +27,29 @@ def test_returns_name(variables, user):
         api_username=variables["api_username"],
         api_key=variables["api_key"]
     )
-    assert user.name in users["name"].values
+    assert users.loc[user.username, "name"] == user.name
 
 
-def test_returns_username(variables, user):
+def test_returns_username_as_index(variables, user):
     users = attendees.attendee_list(
         usernames=[user.username],
         api_username=variables["api_username"],
         api_key=variables["api_key"]
     )
-    assert user.username in users["username"].values
+    assert user.username in users.index
+
+
+def test_returns_avatar_url(variables, user):
+    users = attendees.attendee_list(
+        usernames=[user.username],
+        api_username=variables["api_username"],
+        api_key=variables["api_key"]
+    )
+    avatar_url = users.loc[user.username, "avatar_url"]
+    assert _valid_image_url(avatar_url)
+    assert user.username in avatar_url
+
+
+def _valid_image_url(url):
+    r = requests.get(url)
+    return r.status_code == 200 and r.headers['content-type'] in VALID_IMAGE_MIME_TYPES
